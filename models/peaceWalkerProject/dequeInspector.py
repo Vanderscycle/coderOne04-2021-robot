1eb7f443b3a062153a2596069f9278ed90a2adb4
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-04-11 11:03:12 -0700

    Created two function to create an empty deque for training and to inspect the desired deque.

--- models/peaceWalkerProject/dequeInspector.py
+++ models/peaceWalkerProject/dequeInspector.py
@@ -4,14 +4,36 @@ import os
 
 REPLAY_MEMORY_SIZE = 50_000
 
-file = 'liquidSun_deque'
 dequeForInspection = deque(maxlen=REPLAY_MEMORY_SIZE)
 
 #TODO deque init (for training)
 # add the option to inspect later 
 # maybe a config file generator too
 # a simple meatl gear for testing trained ai (needs only arrayVision)
-with open(os.path.join(os.path.dirname(__file__),f'{file}'),'rb') as dequeFile:
-    dequeForInspection = pickle.load(dequeFile)
 
-    print(dequeForInspection)
+
+def emptyDeque(fileName):
+    """
+    init an empty deque for the nn
+    """
+    emptyDequeObj = deque(maxlen=REPLAY_MEMORY_SIZE)
+    
+    with open(os.path.join(os.path.dirname(__file__),f'{fileName}'),'wb') as dequeFile:
+        pickle.dump(emptyDequeObj,dequeFile)
+
+
+def dequeInspector(fileName):
+    """
+    Take the name of deque file adn return the deque.
+    """
+    with open(os.path.join(os.path.dirname(__file__),f'{fileName}'),'rb') as dequeFile:
+        dequeForInspection = pickle.load(dequeFile)
+
+        return dequeForInspection
+
+
+if __name__ == '__main__':
+    
+    file = 'liquidSun_deque'
+    #emptyDeque(file)
+    print(dequeInspector(file))
