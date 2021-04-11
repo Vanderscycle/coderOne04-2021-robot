ffe050d4b01558f70a82cf90b134ec3efbe5dd0f
Henri Vandersleyen <hvandersleyen@gmail.com>
2021-04-11 11:01:57 -0700

    Added and tested successfully the explosion mapping and have associated reward if the enemy agent is hit by either his or our bombs.

--- modular_agent/metalGearZEKE.py
+++ modular_agent/metalGearZEKE.py
@@ -11,6 +11,8 @@ from models.peaceWalkerProject.liquidSun import AINetMK1,arenaGridEncoder
 import torch
 import os
 import json
+from copy import deepcopy
+
 
 with open(os.path.join(os.path.dirname(__file__),"metalGearZekeConfig.json"),'r') as json_data_file:
     # note json text need a space between key: value
@@ -29,6 +31,7 @@ info = trainingConfig['general_settings']['info']
 class Agent:
 
     actions = {0:'',1:'u',2:'d',3:'l',4:'r',5:'p'}
+    encoderDict = {None:0,'X':1,'E':2,'b':3,'a':4,'t':5,'ob':6,'sb':7,'id':8}
 
 
     def __init__(self):
@@ -36,8 +39,9 @@ class Agent:
         Example of a self, player_num, env)random agent
         """
         # tracked states for data in reinforcement learning
-        self.lastAction = ''
+        self.lastAction = 0
         self.currentState = None
+        self.currentStateNotEncoded = None
         self.lastReward = 0
         #session reward
         self.reward = 0
@@ -48,6 +52,16 @@ class Agent:
         self.actionEpsilon = None
         # need to create method that can calculate when the agents picks a bomb.
         self.bombCount = 3 # init value, we start with three
+        # using GPU for training
+        if torch.cuda.is_available():  
+            print('GPU available, using GPU')
+            self.device = "cuda:0" 
+        else:  
+            print('No GPU available, using CPU')
+            self.device = "cpu"  
+        # optimizing the NN
+        self.policy_net = AINetMK1().to(self.device)
+        self.target_net = AINetMK1().to(self.device)
         # init Zeke's NN
         self.ai = AINetMK1()
         # load the weight
@@ -67,9 +81,6 @@ class Agent:
         Anyway, at the moment the agent selects an action at random.
         """
                 
-        self.reward = self.rewardSystem(player_state)
-        self.episodeReward += self.reward
-        
         # measuring the speed of our program the goal being faster than 1e-3 seconds (100ms)
         begin = time()  
 
@@ -78,8 +89,13 @@ class Agent:
 
         # at the start of the game there is no previous state or action 
         if game_state.tick_number == 0:
+            print('Start of the game. setting past value to current for this once only')
             self.currentState = encodedGrid
+            self.currentStateNotEncoded = self.arrayVision(game_state,player_state)
 
+        self.reward = self.rewardSystem(player_state,game_state)
+        self.episodeReward += self.reward
+        
         # hacked the game files slightly to allow for one more round after game over to use the over flag to save the weights
         if game_state.is_over == True:
             # last action may have to be a number and not a string. double check rewards.
@@ -123,9 +139,10 @@ class Agent:
             # also want to add a reward everytime he picks a bomb
             print(f'agent hp:{player_state.hp}, ammo: {player_state.ammo} bomb Power:{player_state.power}, current reward state: {self.reward} total reward {self.episodeReward}')
             print(f'size of the data {len(self.ai.replay_memory)}')
-        
-        self.lastAction = action
+         
+        self.lastAction = getKey(action,Agent.actions)
         self.currentState = encodedGrid
+        self.currentStateNotEncoded = self.arrayVision(game_state,player_state)
         self.lastReward = self.reward
         return action
 
@@ -150,7 +167,7 @@ class Agent:
             json.dump(trainingConfig,json_data_file)
         
 
-    def rewardSystem(self, player_state):
+    def rewardSystem(self, player_state,game_state):
         """
         Reward system based on ammo picked and damage received.
         
@@ -180,6 +197,8 @@ class Agent:
         # updating the bomb count
         self.bombCount = player_state.ammo
 
+        if self.last2FramesSubstraction(deepcopy(self.currentStateNotEncoded),self.arrayVision(game_state, player_state),info=True):
+            reward += 150
         return reward
 
     
@@ -262,3 +281,155 @@ class Agent:
 
         # also because we only have 100ms I am curious to see the time it takes to execute the action check (https://github.com/pyutils/line_profiler)
         return visionArray
+
+
+    def last2FramesSubstraction(self, previousFrame, currentFrame, info=False, debug=False):
+        """
+        function that maps the blast radius of bombs and return if the ennemy agent was hit
+        """
+        explosionList = list()
+        if debug:
+            print('previousFrame')
+            print(previousFrame)
+            print('currentFrame')
+            print(currentFrame)
+        for rowidx, row in enumerate(currentFrame):
+            for colidx, col in enumerate(row):
+
+                if (currentFrame[rowidx][colidx] is None) and (previousFrame[rowidx][colidx] == 'b'):
+                    # need to add x + y of what was touched (+2)
+                    # also since there will be power ups we need to track who places what bombs
+                    currentFrame[rowidx][colidx] = 'BOOM'
+                    print(f'explosion at (y:{9 - rowidx},x:{colidx})')
+                    explosionList.append((9 - rowidx,colidx))
+                    #print(explosionList)
+
+                elif (currentFrame[rowidx][colidx] in ['E','X','id','sb']):
+                    pass
+
+                elif currentFrame[rowidx][colidx] is None and previousFrame[rowidx][colidx]!='b':
+                    # could be sb,X,E taht moved
+                    currentFrame[rowidx][colidx] = ''
+
+                elif previousFrame[rowidx][colidx] == currentFrame[rowidx][colidx]:
+                    #print(currentFrame[rowidx][colidx])
+                    currentFrame[rowidx][colidx] = currentFrame[rowidx][colidx].replace(previousFrame[rowidx][colidx],'')
+
+        for explosion in explosionList:
+
+            # y boundaries
+            minRow = 0
+            maxRow = 9
+            # x boundaries
+            minCol = 0
+            maxCol = 11
+            # recap: done this way because of how numpy arrays are laid out
+            # also we need to inverse row indexing so that's why we use maxrow - row
+            # frame[row or y][col or x]
+
+            # row edge cases (y coord)
+            if explosion[0] - 3 < 0:
+                minRow = 0
+            else:
+                minRow = explosion[0]-3
+            
+            if explosion[0] + 3 > 9:
+                # 10 instead of 9 (the actual limit) because range ignores the last value
+                maxRow = 10
+            else:
+                maxRow = explosion[0] + 3
+            
+            # columns edge cases (x coord)
+            if explosion[1] - 2 < 0:
+                minCol = 0
+            else:
+                minCol = explosion[1]-2
+            
+            if explosion[1] + 3 > 11:
+                # 12 instead of 11 (the actual limit) because range ignores the last value
+                maxCol = 12
+            else:
+                maxCol = explosion[1] + 3
+
+            #print(minCol,explosion[1],maxCol)
+            #print(minRow,explosion[0],maxRow)
+            
+            # mapping the rows blast
+            for x in range(explosion[0], maxRow):
+
+                # there's a wall at the above the explosion
+                if (currentFrame[9-x][explosion[1]] in ['id','sb']): 
+                    break
+
+                # empty space so the explosion propagate
+                elif currentFrame[9-x][explosion[1]] == '': 
+                    currentFrame[9-x][explosion[1]] = 'bl'
+                # there's an player that was touched by the explosion
+                else:
+                    if currentFrame[9-x][explosion[1]][0]=='[' and currentFrame[9-x][explosion[1]][-1] ==']':
+                        pass
+                    else:
+                        currentFrame[9-x][explosion[1]] = '['+currentFrame[9-x][explosion[1]]+']'
+
+            for x in range(explosion[0], minRow,-1):
+
+                # there's a wall at bellow explosion
+                if (currentFrame[9-x][explosion[1]] in ['id','sb']): 
+                    break
+
+                # empty space so the explosion propagate
+                elif currentFrame[9-x][explosion[1]] == '': 
+                    currentFrame[9-x][explosion[1]] = 'bl'
+                # there's an player that was touched by the explosion
+                else:
+                    if currentFrame[9-x][explosion[1]][0]=='[' and currentFrame[9-x][explosion[1]][-1] ==']':
+                        pass
+                    else:
+                        currentFrame[9-x][explosion[1]] = '['+currentFrame[9-x][explosion[1]]+']'
+
+            # mapping the columns blast
+            for y in range(explosion[1], maxCol):
+                # there's a wall at the right of the explosion
+                if currentFrame[9-explosion[0]][y] in ['id','sb']:
+                    break
+                # empty space so the explosion propagate
+                elif currentFrame[9-explosion[0]][y] == '': 
+                    currentFrame[9-explosion[0]][y] = 'bl'
+                # there's an player that was touched by the explosion
+                else:
+                    if currentFrame[9-explosion[0]][y][0] == '[' and currentFrame[9-explosion[0]][y][-1] == ']':
+                        pass
+                    else:
+                        currentFrame[9-explosion[0]][y] = '['+currentFrame[9-explosion[0]][y]+']'
+
+            for y in range(explosion[1], minCol -1,-1):
+                if currentFrame[9-explosion[0]][y]  in ['id','sb']:
+                    break
+                # empty space so the explosion propagate
+                elif currentFrame[9-explosion[0]][y] == '': 
+                    currentFrame[9-explosion[0]][y] = 'bl'
+                # there's an player that was touched by the explosion
+                else:
+                    if currentFrame[9-explosion[0]][y][0] == '[' and currentFrame[9-explosion[0]][y][-1] == ']':
+                        pass
+                    else:
+                        currentFrame[9-explosion[0]][y] = '['+currentFrame[9-explosion[0]][y]+']'
+        ennemyAgentHit = np.where(currentFrame == '[E]')
+        if info:
+            print(f'explosion location {explosionList}')
+            print(f'enemyhit? :{ennemyAgentHit}')
+            print(currentFrame)
+        if np.size(ennemyAgentHit):
+            return True
+        return False
+
+
+
+# helper functions
+def getKey(val, my_dict):
+    """
+    from https://www.geeksforgeeks.org/python-get-key-from-value-in-dictionary/
+    """
+    for key, value in my_dict.items():
+         if val == value:
+             return key
